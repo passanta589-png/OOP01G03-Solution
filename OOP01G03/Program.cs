f8d8dc79a2fadd9d9382b4680bbd47a21444c3cc
passant ahmed <passantahmed992006@outlook.com>
2026-09-13 20:57:24 +0300

    Q02

--- OOP01G03/Program.cs
+++ OOP01G03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OOP01G03
+﻿using System.Security.Principal;
+
+namespace OOP01G03
 {
     internal class Program
     {
@@ -6,13 +8,27 @@
         {
             #region 01
 
-           //a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
-           //    reason :  يتم نسخ البيانات من متغير DeliveryAddress إلى متغير آخر، وعند تعديل النسخة، فإن التغييرات لا تؤثر على المتغير الأصلي.هذا يحدث لأن DeliveryAddress هو نوع قيمة(value type) وليس نوع مرجعي(reference type).عند نسخ قيمة، يتم إنشاء نسخة مستقلة من البيانات، وبالتالي أي تعديل على النسخة لا يغير البيانات الأصلية.   
-                
-           //b) What happens when a Customer variable is copied into another variable and the copy is modified ?
-           //    reason : عند نسخ متغير Customer إلى متغير آخر، يتم نسخ المرجع إلى نفس الكائن في الذاكرة. لذلك، عند تعديل النسخة، فإن التغييرات تؤثر على المتغير الأصلي أيضًا.هذا يحدث لأن Customer هو نوع مرجعي(reference type)، وعند نسخ المرجع، يتم الإشارة إلى نفس الكائن في الذاكرة، وبالتالي أي تعديل على النسخة يغير البيانات الأصلية أيضًا.
+            //a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            //    reason :  يتم نسخ البيانات من متغير DeliveryAddress إلى متغير آخر، وعند تعديل النسخة، فإن التغييرات لا تؤثر على المتغير الأصلي.هذا يحدث لأن DeliveryAddress هو نوع قيمة(value type) وليس نوع مرجعي(reference type).عند نسخ قيمة، يتم إنشاء نسخة مستقلة من البيانات، وبالتالي أي تعديل على النسخة لا يغير البيانات الأصلية.   
+
+            //b) What happens when a Customer variable is copied into another variable and the copy is modified ?
+            //    reason : عند نسخ متغير Customer إلى متغير آخر، يتم نسخ المرجع إلى نفس الكائن في الذاكرة. لذلك، عند تعديل النسخة، فإن التغييرات تؤثر على المتغير الأصلي أيضًا.هذا يحدث لأن Customer هو نوع مرجعي(reference type)، وعند نسخ المرجع، يتم الإشارة إلى نفس الكائن في الذاكرة، وبالتالي أي تعديل على النسخة يغير البيانات الأصلية أيضًا.
             #endregion
-        }
+
+            #region 02
+
+            //a) Identify at least three problems with this design from an encapsulation perspective.
+            //1) public fields: The class has public fields, 
+            //2)no validation 
+            //3)violation of encapsulation
+
+            //b)b) How can private fields and public properties improve this design?
+            // 1) protects data
+            //2)improves maintainability
+            //3) Reduced bugs
+            //4)provides flexibility
+        #endregion
+    }
     }
 }
 
